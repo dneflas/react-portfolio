d655db4a9a1d0a11ea8c0b5613b9d61eb0abac3a
Debbie Neflas <debbieneflas@gmail.com>
2026-05-06 22:16:56 -0700

    update contact page

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { validateEmail, capitalizeFirstLetter } from "../../utils/helpers";
 import { send } from "@emailjs/browser";
+import "./ContactForm.css";
 
 function ContactForm() {
   const [errorMessage, setErrorMessage] = useState("");
@@ -11,21 +12,6 @@ function ContactForm() {
     email: "",
     message: "",
   });
-
-  const [contactLinks] = useState([
-    {
-      name: "Email",
-      link: "mailto:debbieneflas@gmail.com",
-      icon: "fa-solid fa-envelope",
-      text: "debbieneflasdev@gmail.com",
-    },
-    // {
-    //   name: "Phone",
-    //   link: "tel:+14242092856",
-    //   icon: "fa-solid fa-phone",
-    //   text: "424.209.2586",
-    // },
-  ]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -56,7 +42,7 @@ function ContactForm() {
       "default_service",
       "template_paa5ipo",
       formState,
-      "46V0eFry4B4d0hEHE"
+      "46V0eFry4B4d0hEHE",
     ).then(
       (response) => {
         console.log("SUCCESS", response.status, response.text);
@@ -65,7 +51,7 @@ function ContactForm() {
       (error) => {
         console.log("FAILED...", error);
         setErrorMessage("Something went wrong!");
-      }
+      },
     );
 
     setFormState({
@@ -76,87 +62,115 @@ function ContactForm() {
     console.log(formState);
   }
 
+  const [contactLinks] = useState([
+    {
+      name: "email",
+      link: "mailto:debbieneflasdev@gmail.com",
+      icon: "fa-solid fa-envelope",
+      info: "debbieneflasdev@gmail.com",
+    },
+    {
+      name: "linkedIn",
+      link: "https://www.linkedin.com/in/debbieneflas/",
+      icon: "fa-brands fa-linkedin",
+      info: "linkedin.com/in/debbieneflas",
+    },
+    {
+      name: "github",
+      link: "https://github.com/dneflas",
+      icon: "fa-brands fa-github",
+      info: "github.com/dneflas",
+    },
+  ]);
+
   const { name, email, message } = formState;
   return (
-    <section className="my-5">
-      <h2>Contact Me</h2>
-      <div className="flex-row justify-center my-auto">
-        {contactLinks.map((link) => (
-          <div className=" mx-4 my-3" key={link.name}>
-            <a href={link.link} className="ml-2 my-1 px-2 py-1 text-dark icons">
-              <i className={link.icon}></i> : {link.text}
-            </a>
-          </div>
-        ))}
-      </div>
-      {emailSent ? (
-        <>
-          <div className="flex-row my-3 justify-center">
-            <div className="col-12 col-md-8 my-5 text-center">
-              <p>
-                Thanks, your form has been submitted. I look forward to
-                connecting soon!
-              </p>
-              <button
-                onClick={() => setEmailSent(false)}
-                className="btn my-5 w-25"
-              >
-                Back to Contact Form
-              </button>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="flex-row my-3 justify-center card">
-          <form className="col-12 col-md-8" onSubmit={handleSubmit}>
-            {errorMessage && (
-              <div>
-                <p className="text-tertiary">{errorMessage}</p>
+    <div className="flex-row justify-space-around my-5">
+      <section className="col-12 col-md-4">
+        <h2>Let's Connect</h2>
+        <p>
+          Whether it’s a big idea or a quick hello, my inbox is always open 😊
+        </p>
+        <ul className="mt-5">
+          {contactLinks.map((link) => (
+            <li key={link.name} className="my-4">
+              <a href={link.link} target="_blank" rel="noopener noreferrer">
+                <i className={link.icon}></i> {link.info}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <div className=" col-12 col-md-7">
+        {emailSent ? (
+          <>
+            <div className="flex-row my-3 justify-center">
+              <div className="col-12 col-md-8 my-5 text-center">
+                <p>
+                  Thanks, your form has been submitted. I look forward to
+                  connecting soon!
+                </p>
+                <button
+                  onClick={() => setEmailSent(false)}
+                  className="btn my-5 w-25"
+                >
+                  Back to Contact Form
+                </button>
               </div>
-            )}
-            <div className="my-1">
-              <label className="form-label" htmlFor="name">
-                Name:{" "}
-              </label>
-              <input
-                className="form-input"
-                type="text"
-                name="name"
-                defaultValue={name}
-                onBlur={handleChange}
-              />
             </div>
-            <div className="my-1">
-              <label className="form-label" htmlFor="email">
-                Email:{" "}
-              </label>
-              <input
-                className="form-input"
-                type="text"
-                name="email"
-                defaultValue={email}
-                onBlur={handleChange}
-              />
-            </div>
-            <div className="my-1">
-              <label className="form-label" htmlFor="message">
-                Message:{" "}
-              </label>
-              <textarea
-                className="form-textarea"
-                name="message"
-                rows="8"
-                defaultValue={message}
-                onBlur={handleChange}
-              />
-            </div>
-            <button className="btn my-1" type="submit">
-              Submit
-            </button>
-          </form>
-        </div>
-      )}
-    </section>
+          </>
+        ) : (
+          <div className=" card">
+            <form className="mx-4" onSubmit={handleSubmit}>
+              {errorMessage && (
+                <div>
+                  <p className="text-tertiary">{errorMessage}</p>
+                </div>
+              )}
+              <div className="my-1">
+                <label className="form-label" htmlFor="name">
+                  Name:{" "}
+                </label>
+                <input
+                  className="form-input"
+                  type="text"
+                  name="name"
+                  defaultValue={name}
+                  onBlur={handleChange}
+                />
+              </div>
+              <div className="my-1">
+                <label className="form-label" htmlFor="email">
+                  Email:{" "}
+                </label>
+                <input
+                  className="form-input"
+                  type="text"
+                  name="email"
+                  defaultValue={email}
+                  onBlur={handleChange}
+                />
+              </div>
+              <div className="my-1">
+                <label className="form-label" htmlFor="message">
+                  Message:{" "}
+                </label>
+                <textarea
+                  className="form-textarea"
+                  name="message"
+                  rows="8"
+                  defaultValue={message}
+                  onBlur={handleChange}
+                />
+              </div>
+              <button className="btn primary my-1" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
 
